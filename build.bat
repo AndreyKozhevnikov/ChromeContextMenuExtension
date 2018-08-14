dbc2567e66da2616dc1a7a58ce2e18f37c4e338f
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-08-14 13:18:01 +0300

    rename openEntityInSC extension (to work with user IDs)

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ node SourceCode\buildCore.js &&^
 mustache Temp\myCoreJSON.js SourceCode\html.js > Extensions\html\htmlScript.js &&^
 mustache Temp\myCoreJSON.js SourceCode\markup.js > Extensions\markup\markupScript.js&&^
 mustache Temp\myCoreJSON.js SourceCode\createTag.js > Extensions\createTagLink\createTagScript.js&&^
-mustache Temp\myCoreJSON.js SourceCode\openTicket.js > Extensions\openTicket\openTicketScript.js &&^
+mustache Temp\myCoreJSON.js SourceCode\openEntityInSC.js > Extensions\openEntityInSC\openEntityInSCscript.js.js &&^
 mustache Temp\myCoreJSON.js SourceCode\plainText.js > Extensions\plainText\plainTextScript.js &&^
 
