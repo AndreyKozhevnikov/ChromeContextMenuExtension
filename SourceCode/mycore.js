27f6828eb7b212557072b6433204e0cceec50f3e
Andrey <andrey.kozhevnikov@devexpress.com>
2021-05-24 05:55:05 +0300

    support 1m ticket

--- SourceCode/mycore.js
+++ SourceCode/mycore.js
@@ -96,7 +96,7 @@ function escapeHTML(text) {
 }
 
 function findTicketNoInText(textToSearch) {
-  let regex = /[TESQKBC]{1,2}A?\d{3,6}/gi;
+  let regex = /[TESQKBC]{1,2}A?\d{3,7}/gi;
   let regexShortA = /(A\d{1,4})\D/gi;
   let results = regex.exec(textToSearch);
   let resultsShortA = regexShortA.exec(textToSearch);
