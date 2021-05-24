99153e317207062fa1fb3e5377b82fb695599715
Andrey <andrey.kozhevnikov@devexpress.com>
2021-05-24 05:54:10 +0300

    lint

--- Test/openEntityInScTests.js
+++ Test/openEntityInScTests.js
@@ -4,7 +4,6 @@ let mycore = require('../SourceCode/mycore.js');
 
 describe('getLinkTitleFromTag', function() {
   it('should get T663889 from T663889: ASPxTreeListEditor - Duplicate node key exception ', function() {
-
     // 1. ARRANGE
     let txt0 = 'test T663889: ASPxTreeListEditor - Duplicate node key exception';
     let txt1 = 'test A2944: XPO Best Practices';
@@ -33,10 +32,8 @@ describe('getLinkTitleFromTag', function() {
 
     // 3. ASSERT
     assert.equal(res, 'A1234567');
-
   });
   it('should get 12123@asdfasdf.com from testtext / 12123@asdfasdf.com sometext ', function() {
-
     // 1. ARRANGE
     let txt = 'testtext / 12123@asdfasdf.com sometext';
 
@@ -45,10 +42,8 @@ describe('getLinkTitleFromTag', function() {
 
     // 3. ASSERT
     assert.equal(res, '12123@asdfasdf.com');
-
   });
   it('should get manoj@mealcast.io from is manoj@mealcast.io. ', function() {
-
     // 1. ARRANGE
     let txt = 'is manoj@mealcast.io.';
 
@@ -57,22 +52,19 @@ describe('getLinkTitleFromTag', function() {
 
     // 3. ASSERT
     assert.equal(res, 'manoj@mealcast.io');
-
   });
   it('should get KA18843 from https://www.devexpress.com/Support/Center/Question/Details/KA18843/how-can-i-debug-devexpress ', function() {
-
     // 1. ARRANGE
-    let txt = 'https://www.devexpress.com/Support/Center/Question/Details/KA18843/how-can-i-debug-devexpress';
+    let txt =
+      'https://www.devexpress.com/Support/Center/Question/Details/KA18843/how-can-i-debug-devexpress';
 
     // 2. ACT
     let res = mycore.findTicketNoInText(txt);
 
     // 3. ASSERT
     assert.equal(res, 'KA18843');
-
   });
   it('should get BC4447 from testtextBC4447testaa ', function() {
-
     // 1. ARRANGE
     let txt = 'testtextBC4447testaa';
 
@@ -81,15 +73,14 @@ describe('getLinkTitleFromTag', function() {
 
     // 3. ASSERT
     assert.equal(res, 'BC4447');
-
   });
 });
 
 describe('getUserIdFromText', function() {
   it('should not find userId from KA18785 ', function() {
-
     // 1. ARRANGE
-    let txt0 = 'https://www.devexpress.com/Support/Center/Question/Details/KA18785/known-security-system-module-scenarios-unsupported-out-of-the-box';
+    let txt0 =
+      'https://www.devexpress.com/Support/Center/Question/Details/KA18785/known-security-system-module-scenarios-unsupported-out-of-the-box';
     // 2. ACT
     let res0 = mycore.findUserIdInText(txt0);
     // 3. ASSERT
@@ -97,7 +88,6 @@ describe('getUserIdFromText', function() {
   });
 
   it('should not find ticketId from A1203557 ', function() {
-
     // 1. ARRANGE
     let txt0 = 'test text A1203557 txt';
     // 2. ACT
@@ -107,23 +97,19 @@ describe('getUserIdFromText', function() {
   });
 
   it('should  find user  from A1203557 ', function() {
-
     // 1. ARRANGE
     let txt0 = 'test text A1203557 txt';
     // 2. ACT
     let res0 = mycore.findUserIdInText(txt0);
     // 3. ASSERT
     assert.equal(res0, 'A1203557');
-
   });
   it('should  find user  from A1203557 ', function() {
-
     // 1. ARRANGE
     let txt0 = 'A1203557';
     // 2. ACT
     let res0 = mycore.findUserIdInText(txt0);
     // 3. ASSERT
     assert.equal(res0, 'A1203557');
-
   });
 });
